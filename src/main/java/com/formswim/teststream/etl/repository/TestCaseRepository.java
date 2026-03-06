40e827bb3864fccb1e7e9126627e9b7eda5dc030
chiar17 <andy.ngo877279@gmail.com>
2026-03-06 15:31:52 -0800

    etl add TestCaseRepository interface

--- src/main/java/com/formswim/teststream/etl/repository/TestCaseRepository.java
+++ src/main/java/com/formswim/teststream/etl/repository/TestCaseRepository.java
@@ -1,5 +1,20 @@
 package com.formswim.teststream.etl.repository;
 
-public class TestCaseRepository {
+import com.formswim.teststream.etl.model.TestCase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TestCaseRepository extends JpaRepository<TestCase, Long> {
+
+    Optional<TestCase> findByWorkKey(String workKey);
+    boolean existsByWorkKey(String workKey);
+    List<TestCase> findByFolder(String folder);
+    List<TestCase> findByStatus(String status);
+
+
     
 }
