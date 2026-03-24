64e9eb197a8e7d7517b13c34cf44fe19a6b2b96d
chiar17 <andy.ngo877279@gmail.com>
2026-03-23 21:50:36 -0700

    fix: add missing import for Arrays in TagController

--- src/main/java/com/formswim/teststream/etl/controller/TagController.java
+++ src/main/java/com/formswim/teststream/etl/controller/TagController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
